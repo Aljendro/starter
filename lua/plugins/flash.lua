6c13e383e16cf2e9621e0d2bf9e3474aa50e759b
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-09-18 17:28:03 -0700

    feat: updating flash.nvim config

--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -1,9 +1,15 @@
 return {
   "folke/flash.nvim",
+  keys = {
+    { "s", false },
+  },
   opts = {
     modes = {
       char = {
         enabled = false,
+      },
+      search = {
+        enabled = true,
       },
     },
   },
