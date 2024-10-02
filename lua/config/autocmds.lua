4c27a12d8bc53468e6404ef8bd98bee84a2aed5f
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-10-02 15:51:36 -0700

    feat: adding autosave to autocommands

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,11 +3,47 @@
 -- Add any additional autocmds here
 
 local customVim = vim.api.nvim_create_augroup("customVim", { clear = true })
+
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   group = customVim,
   pattern = "*",
   callback = function()
     vim.opt_local.formatoptions:remove({ "o" })
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
+  group = customVim,
+  pattern = table.concat({
+    "*.edn",
+    "*.clj",
+    "*.cljc",
+    "*.cljs",
+    "*.css",
+    "*.fnl",
+    "*.go",
+    "*.html",
+    "*.js",
+    "*.json",
+    "*.jsonl",
+    "*.jsx",
+    "*.lua",
+    "*.md",
+    "*.py",
+    "*.rs",
+    "*.ts",
+    "*.tsx",
+    "*.txt",
+    "*.yaml",
+    "*.yml",
+    "*.hbs",
+    "*.cpp",
+    "*.c",
+    "*.h",
+    "*.hpp",
+  }, ","),
+  callback = function()
+    vim.cmd("silent! w")
   end,
 })
 
