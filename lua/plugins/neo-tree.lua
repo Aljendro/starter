a80c60e70b7bccd349d0055c3b131a9bf33a6327
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2024-12-14 13:48:55 -0800

    feat: allow cwd tracking for neotree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,6 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
+      bind_to_cwd = true,
       filtered_items = {
         hide_dotfiles = false,
       },
@@ -19,7 +20,7 @@ return {
         ["t"] = "",
         ["g?"] = "show_help",
         ["<C-v>"] = "open_vsplit",
-        ["<C-x>"] = "open_split",
+        ["<C-s>"] = "open_split",
         ["<C-t>"] = "open_tabnew",
       },
     },
