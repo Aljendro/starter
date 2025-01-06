c4e0e4b8940afbce127518375f5058292008cabc
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-01-06 11:53:27 -0800

    fix: removing bad virtual_text config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -46,12 +46,3 @@ vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
     vim.cmd("silent! w")
   end,
 })
-
-vim.api.nvim_create_user_command("DiagnosticsToggleVirtualText", function()
-  local current_value = vim.diagnostic.config().virtual_text
-  if current_value then
-    vim.diagnostic.config({ virtual_text = false })
-  else
-    vim.diagnostic.config({ virtual_text = true })
-  end
-end, {})
