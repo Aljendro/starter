373db5eec45adf1e65463175ff0d5afa644ba03e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-01-05 22:05:53 -0800

    feat: updating keybinds for fzf-lua

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     filesystem = {
-      bind_to_cwd = true,
+      bind_to_cwd = false,
       filtered_items = {
         hide_dotfiles = false,
       },
