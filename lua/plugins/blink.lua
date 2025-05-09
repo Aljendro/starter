8c4b6df17214ce37d70e4ad8f696a3a3094db78e
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-05-09 11:16:10 -0700

    feat: reduce copilot score offset

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -3,4 +3,11 @@ return {
   dependencies = {
     { "rafamadriz/friendly-snippets", dev = true },
   },
+  opts = function(_, opts)
+    for name, provider in pairs(opts.sources.providers) do
+      if name == "copilot" then
+        provider.score_offset = 10
+      end
+    end
+  end,
 }
