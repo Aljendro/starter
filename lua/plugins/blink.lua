df79909baf9944dcd6b8f269eff13d2681669e4a
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-06-12 20:25:22 -0700

    fix: removing source changes for blink.lua

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,21 +2,5 @@ return {
   "saghen/blink.cmp",
   dependencies = {
     { "rafamadriz/friendly-snippets", dev = true },
-  },
-  -- opts = function(_, opts)
-  --   for name, provider in pairs(opts.sources.providers) do
-  --     if name == "copilot" then
-  --       provider.score_offset = 10
-  --     end
-  --   end
-  -- end,
-  opts = {
-    sources = {
-      providers = {
-        snippets = {
-          score_offset = 200,
-        },
-      },
-    },
-  },
+  }
 }
