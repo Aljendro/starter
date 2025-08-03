20640afd9cf30ac231de83e0e974ff0cd7da5e8c
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-08-03 13:21:59 -0700

    feat: upgrading to node 24

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,7 +4,7 @@
 
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
-vim.g.node_host_prog = vim.fn.expand("~/.nvm/versions/node/v20.15.1/bin/neovim-node-host")
+vim.g.node_host_prog = vim.fn.expand("~/.nvm/versions/node/v24.5.0/bin/neovim-node-host")
 vim.g.python3_host_prog = vim.fn.expand("/usr/bin/python3")
 
 vim.opt.wrapscan = false
