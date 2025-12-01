da22cbbdde0821c21d2175fb6d269070a96f1642
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2025-11-30 19:40:48 -0800

    feat: removing unused options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -11,14 +11,3 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
     vim.opt_local.formatoptions:remove({ "o" })
   end,
 })
-
--- vim.api.nvim_create_autocmd({ "TextChanged", "TextChangedI" }, {
---   group = customVim,
---   pattern = "*",
---   callback = function()
---     if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
---       vim.cmd("silent! write")
---     end
---   end,
---   desc = "Auto-save after text change",
--- })
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,8 +4,8 @@
 
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
 vim.g.node_host_prog = vim.fn.expand("~/.nvm/versions/node/v24.5.0/bin/neovim-node-host")
-vim.g.python3_host_prog = vim.fn.expand("/usr/bin/python3")
 
 vim.opt.wrapscan = false
 
