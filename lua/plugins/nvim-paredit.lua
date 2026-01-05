e5c125d0d2af9b11321165b46e1906248b5ae8c5
Alejandro Alvarado <alejandro.alvarado0650144@gmail.com>
2026-01-05 11:10:14 -0800

    feat: removing paredit

--- lua/plugins/nvim-paredit.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "julienvincent/nvim-paredit",
-  dependencies = {
-    { "PaterJason/nvim-treesitter-sexp", enabled = false },
-  },
-  config = function()
-    require("nvim-paredit").setup()
-  end,
-}
